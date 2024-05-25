712a5e56d7f1568470d84c2f831b0e64aab0c714
Parham Sabetkar <parham.sabetkar@gmail.com>
2024-05-25 11:23:16 +0330

    Created models and modified admin page for types_and_features app

--- types_and_features/admin.py
+++ types_and_features/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.admin import register
+
+from .models import TypeOfRestaurant, UniqueFeature, CommonFeature, InternationalFoodType, MainFoodType
+
 
 # Register your models here.
+@register(TypeOfRestaurant)
+class TypeOfRestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type')
+    list_display_links = ('type',)
+    search_fields = ('type',)
+
+
+@register(UniqueFeature)
+class UniqueFeatureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'unique_feature_name')
+    list_display_links = ('unique_feature_name',)
+    search_fields = ('unique_feature_name',)
+
+
+@register(CommonFeature)
+class CommonFeatureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'common_feature_name')
+    list_display_links = ('common_feature_name',)
+    search_fields = ('common_feature_name',)
+
+
+@register(InternationalFoodType)
+class InternationalFoodTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'international_type')
+    list_display_links = ('international_type',)
+    search_fields = ('international_type',)
+
+
+@register(MainFoodType)
+class MainFoodTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'main_type')
+    list_display_links = ('main_type',)
+    search_fields = ('main_type',)
--- types_and_features/models.py
+++ types_and_features/models.py
@@ -1,3 +1,92 @@
 from django.db import models
 
+
 # Create your models here.
+class TypeOfRestaurant(models.Model):
+    type = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        verbose_name='Type',
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Type of Restaurant'
+        verbose_name_plural = 'Type of Restaurants'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return self.type
+
+
+class UniqueFeature(models.Model):
+    unique_feature_name = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        verbose_name='Unique Feature Name',
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Unique Feature'
+        verbose_name_plural = 'Unique Features'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return self.unique_feature_name
+
+
+class CommonFeature(models.Model):
+    common_feature_name = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        verbose_name='Common Feature Name',
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Common Feature'
+        verbose_name_plural = 'Common Features'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return self.common_feature_name
+
+
+class InternationalFoodType(models.Model):
+    international_type = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        verbose_name='International Type',
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'International Food Type'
+        verbose_name_plural = 'International Food Types'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return self.international_type
+
+
+class MainFoodType(models.Model):
+    main_type = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        verbose_name='Main Type',
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Main Food Type'
+        verbose_name_plural = 'Main Food Types'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return self.main_type
