3f277cddf5d09210ab0d940de620fc416136b791
Parham Sabetkar <parham.sabetkar@gmail.com>
2024-06-16 14:02:47 +0330

    Modified error handling for sign up form in backend

--- restaurant_area/models.py
+++ restaurant_area/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.conf import settings
+from django.core.validators import URLValidator
 
 from lib.common_base_models import BaseModel, BaseModelWithIsActive
 from types_and_features.models import (TypeOfRestaurant, UniqueFeature, CommonFeature, InternationalFoodType,
@@ -24,7 +25,7 @@ class Restaurant(BaseModelWithIsActive):
     address = models.TextField(null=False, blank=False, verbose_name='Address')
     mobile_number = models.CharField(max_length=11, null=False, blank=False, verbose_name='Mobile Number')
     landline_number = models.CharField(max_length=8, null=False, blank=False, verbose_name='Landline Number')
-    link = models.URLField(null=True, blank=True, verbose_name='Link')
+    link = models.URLField(null=True, blank=True, validators=[URLValidator()], verbose_name='Link')
     social_media = models.CharField(
         max_length=10,
         choices=settings.SOCIAL_MEDIA_CHOICES,
